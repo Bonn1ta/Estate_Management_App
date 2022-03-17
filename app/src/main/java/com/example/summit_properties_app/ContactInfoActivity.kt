937137147f450c9ac1b70e28f926494c9687bd2a
Bonnita Jansen <bventer@retrorabbit.co.za>
2022-03-17 21:33:05 +0200

    making a call

--- app/src/main/java/com/example/summit_properties_app/ContactInfoActivity.kt
+++ app/src/main/java/com/example/summit_properties_app/ContactInfoActivity.kt
@@ -6,10 +6,14 @@ import android.os.Bundle
 import android.view.View
 
 class ContactInfoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_info)
     }
+
+    //private static final int REQUEST_CALL = 1
+
     var propertyManagementContacts = arrayOf("Amelia Chamberlain", "Jason Vaughan", "Jamal Rankin")
     var communityManagementContacts = arrayOf("Eliott Stewart", "Sofie Flynn", "Hassan Salas", "Alayna Griffith", "Jazmine Rooney")
     var importantContact = arrayOf("Thomas White", "Cyrus Rooney", "Corey Stuart")
